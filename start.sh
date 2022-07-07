8d2d9344ae266880ee899f15445c77a7007392fa
ƁROƘƐƝ  HƐARƬ <107612506+HEARTBEAT-77@users.noreply.github.com>
2022-07-07 18:28:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/HEARTBEAT-77/RadioPlayerV3.git /RadioPlayerV3
-echo "Installing Requirements..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning Repo, Please Wait..."
+  git clone https://github.com/HEARTBEAT-77/RadioPlayerV3.git /RadioPlayerV3
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /RadioPlayerV3
+fi
 cd /RadioPlayerV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
